537026fd138a8fb839eb65fd43f073eeeff3401d
Thomson, Alec (CASS, Kensington) <alec.thomson@csiro.au>
2023-07-26 21:32:17 +1000

    Formatting

--- make_racs.py
+++ make_racs.py
@@ -3,19 +3,22 @@
 """
 Convert ASKAP long track data to 'RACS' data
 """
-from casatasks import split
-from casacore.tables import table, taql
-from pathlib import Path
-import numpy as np
-import matplotlib.pyplot as plt
-from astropy.time import Time
-import astropy.units as u
 import shutil
-from tqdm.auto import tqdm
-
+from pathlib import Path
 from typing import Tuple
 
-def find_meridian_time(ms: Path, integration_time: u.Quantity = 15 * u.min) -> Tuple[Time, Time]:
+import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.time import Time
+from casacore.tables import table, taql
+from casatasks import split
+from tqdm.auto import tqdm
+
+
+def find_meridian_time(
+    ms: Path, integration_time: u.Quantity = 15 * u.min
+) -> Tuple[Time, Time]:
     """Find the time around the meridian for a given MS and integration time
 
     Args:
@@ -24,18 +27,21 @@ def find_meridian_time(ms: Path, integration_time: u.Quantity = 15 * u.min) -> T
 
     Returns:
         Tuple[Time, Time]: Start and end times around meridian
-    """    
+    """
     with table((ms / "POINTING").as_posix(), ack=False) as tab:
         # Get ELEVATION for antenna 0
-        sels = taql("select ELEVATION, TIME from $tab where ANTENNA_ID == 0 and TRACKING == True")
+        sels = taql(
+            "select ELEVATION, TIME from $tab where ANTENNA_ID == 0 and TRACKING == True"
+        )
         elevation = sels.getcol("ELEVATION") * u.deg
         times = Time(sels.getcol("TIME") * u.s, format="mjd")
-    
+
     # Find times around meridian
     peak_time = times[np.argmax(elevation)]
-    start = (peak_time - integration_time/2)
-    end = (peak_time + integration_time/2)
+    start = peak_time - integration_time / 2
+    end = peak_time + integration_time / 2
     return start, end
+
 
 def long_to_racs(
     ms: Path,
@@ -47,10 +53,10 @@ def long_to_racs(
 
     Args:
         ms (Path): Long track MS
-        start (Time): Start time 
+        start (Time): Start time
         end (Time): End time
         field_prefix (str, optional): Field name prefix. Defaults to "EMU".
-    """    
+    """
     outf = Path(ms.as_posix().replace(field_prefix, "RACS"))
     print(f"Splitting {ms.name} to {outf.name}")
     if outf.exists():
@@ -94,16 +100,24 @@ def make_racs(
 
 def cli():
     import argparse
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("data_dir", type=Path)
-    parser.add_argument("--integration_time", type=float, default=15, help="Integration time in minutes")
-    parser.add_argument("--field_prefix", type=str, default="EMU", help="Field name prefix")
+    parser.add_argument(
+        "--integration_time", type=float, default=15, help="Integration time in minutes"
+    )
+    parser.add_argument(
+        "--field_prefix", type=str, default="EMU", help="Field name prefix"
+    )
     args = parser.parse_args()
     make_racs(
         data_dir=Path(args.data_dir),
         integration_time=args.integration_time * u.min,
         field_prefix=args.field_prefix,
     )
+
 
 if __name__ == "__main__":
     cli()
